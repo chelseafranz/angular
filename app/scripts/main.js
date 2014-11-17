36e4666ac7ad05a461bed3e88b843a839017e2bc
Chelsea Franz <chelseafranz3@gmail.com>
2014-11-17 11:47:42 -0500

    added person controller, download angular

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
-console.log('The Iron Yard Rocks');
+var app= angular.module('PeopleList', []);
+
+// app.config( function($routeProvider){
+
+// 	$routeProvider.when('/', {
+// 		//set controller, view, all kinds of stuff
+// 	});
+
+// });
+
+app.controller('PersonController', function($scope){
+	//anything we want to have access to must be a usable asset inside of function
+
+	// $scope.person = {
+	// 	name: 'Chelsea',
+	// 	age: 25
+	// };
+
+	$scope.people = [
+	{
+		name: 'Chelsea',
+		age: 25
+	},
+		{
+		name: 'Jo',
+		age: 109
+	}
+	];
+
+	$scope.hello= function (n,i){
+		if($scope.people[i].name === 'Chelsea'){
+			alert('hey');
+		}else{
+		alert('hi ' + n);
+	}
+	};
+
+});
